40d1860f3c7f46e112340a3effe0e06a8bc20ef5
jeremyoha450 <tenderlover_1999@yahoo.com>
2026-06-24 17:09:23 +1000

    Create provider-settings.js

---
+++ scripts/providers/provider-settings.js
@@ -0,0 +1,13 @@
+// providers/provider-settings.js
+
+export const providerSettings = {
+
+    endpoint:
+        "http://192.168.15.5:5002/v1",
+
+    apiKey:
+        "Koboldccp",
+
+    model:
+        "gemma-4-E4B-it-uncensored-Q4_K_M"
+};
