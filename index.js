2a101f408cab0da5f028e8c273a65bdb1a25b45e
jeremyoha450 <tenderlover_1999@yahoo.com>
2026-06-24 17:06:12 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
+import { initUI }
+from "./scripts/ui.js";
+
 import {
-    createCharacter,
-    addCharacter,
-    getDatabase,
-} from "./scripts/database.js";
+    initializeContinuityManager
+} from "./scripts/continuity-manager.js";
 
-jQuery(async () => {
-    console.log("Character Continuity Manager Loaded");
+console.log(
+    "=== CCM STARTING ==="
+);
 
-    const db = getDatabase();
+jQuery(() => {
+    console.log(
+        "Character Continuity Manager Loaded"
+    );
 
-    console.log(db);
-
-    if (Object.keys(db.characters).length === 0) {
-        const sarah = createCharacter("Sarah");
-
-        addCharacter(sarah);
-
-        console.log("Created Sarah");
-    }
+    initUI();
+    initializeContinuityManager();
 });
